06bebdc3f1884d23426e5a886b1ddc61059e3293
ehaley3 <ehaley3@nd.edu>
2025-12-12 22:27:25 -0500

    Create data_prep.R
    Created a script file where we can do any data prep. Feel free to remove this if you want

---
+++ East_Group2_App/data_prep.R
@@ -0,0 +1,11 @@
+calls <- read.csv("311_Phone_Call_Log_Mod.csv")
+licenses <- read.csv("Business_Licenses.csv")
+parks <- read.csv("Parks_Locations_and_Features.csv")
+facilities <- read.csv("Public_Facilities.csv")
+
+
+facilities <- facilities %>%
+  mutate(
+    POPL_ADDR1 = gsub("\r\n", ", ", POPL_ADDR1),
+    POPL_ZIP = as.character(POPL_ZIP)
+  )
