46dcf3709bffad1a6a1b43cbc2a42ed72746dc31
Bridget Liesman <bliesman@nd.edu>
2025-12-13 18:14:22 -0600

    Add code to my tab (Crime)

--- East_Group2_App/app.R
+++ East_Group2_App/app.R
@@ -2,35 +2,54 @@ library(shiny)
 library(tidyverse)
 library(DT)
 
-
-source("data_prep.R", local = TRUE)
-
-#calls <- read.csv("311_Phone_Call_Log_Mod.csv")
-#licenses <- read.csv("Business_Licenses.csv")
-#parks <- read.csv("Parks_Locations_and_Features.csv")
-#facilities <- read.csv("Public_Facilities.csv")
-
-
+#bring in prepped data
+source("data_prep.R")
 
 ui <- fluidPage(
-  titlePanel("City of South Bend"),
+  titlePanel("City of South Bend Parks"),
   
   tabsetPanel(
+    #Bridget's work ---------------------------------------------------------------------------------------------
     tabPanel(
-      "311 Phone Call Log",
-      h3("311 Phone Call Log"),
+      "Crime Data",
+      h3("Crime Map"),
+      h4("How much crime occurs around each park?"),
+      sidebarLayout(
+        sidebarPanel(
+          #park chooser
+          selectInput(inputId = "park_select", label = "Select Park(s):",
+            choices = parks_proj$Park_Name, selected = NULL, multiple = TRUE),
+          #slider for crime radius
+          sliderInput(inputId = "radius", label = "Distance from Park (m):", 
+                      min = 100, max = 1000, value = 500, step = 50),
+          #inputs for date range
+          dateRangeInput(inputId = "date_range", label = "Select date range:",
+                         start = min(crime_simpleB$date), end = max(crime_simpleB$date),
+                         min = min(crime_proj$date), max = max(crime_proj$date)),
+          #commonly used start/end periods
+          actionButton("all_time", "All Time"),
+          actionButton("prev_year", "Previous Year"),
+          actionButton("ytd", "Year to Date"),
+          actionButton("mtd", "Month to Date")
+        ),
+        mainPanel(leafletOutput("park_map", height = 600),
+                  plotOutput("crime_timeseries"))
+      ),
       DT::dataTableOutput("table1")
     ),
+    #Danielle's work ---------------------------------------------------------------------------------------------
     tabPanel(
       "Business Licenses",
       h3("Business Licenses"),
       DT::dataTableOutput("table2")
     ),
+    #Jake's work ---------------------------------------------------------------------------------------------
     tabPanel(
       "Parks",
       h3("Parks"),
       DT::dataTableOutput("table3")
     ),
+    #Erich's work ---------------------------------------------------------------------------------------------
     tabPanel(
       "Public Facilities",
       h3("Public Facilities"),
@@ -39,11 +58,85 @@ ui <- fluidPage(
   )
 )
 
-server <- function(input, output) {
-  output$table1 <- DT::renderDataTable(calls)
+server <- function(input, output, session) {
+  
+  ###Bridget - tab1 ---------------------------------------------------------------------------------------------
+  #create commonly used start/end dates
+  #all time
+  observeEvent(input$all_time, {
+    updateDateRangeInput(session, "date_range",
+                         start = min(crime_proj$date),
+                         end = max(crime_proj$date))
+  })
+  #previous year
+  observeEvent(input$prev_year, {
+    updateDateRangeInput(session, "date_range",
+                         start = max(crime_proj$date) - 365,
+                         end = max(crime_proj$date))
+  })
+  #ytd
+  observeEvent(input$ytd, {
+    updateDateRangeInput(session, "date_range",
+                         #start = as.Date(paste0(format(max(crime_proj$date), "%Y"), "-01-01")),
+                         start = as.Date(format(max(crime_proj$date), "%Y-01-01")),
+                         end = max(crime_proj$date))
+  })
+  #mtd
+  observeEvent(input$mtd, {
+    updateDateRangeInput(session, "date_range",
+                         start = as.Date(format(max(crime_proj$date), "%Y-%m-01")),
+                         end = max(crime_proj$date))
+  })
+  
+  #create crime map using dynamic radius and time range
+  output$park_map <- renderLeaflet({
+    parks_map <- generate_crime_map(crime_radius = input$radius, 
+                                    start_date = input$date_range[1], end_date = input$date_range[2])
+    
+    #create colors depending on crime count
+    pal <- colorNumeric("Reds", domain = parks_map$crime_count)
+    
+    #create leaflet with crime data
+    leaflet(parks_map) %>%
+      addProviderTiles("CartoDB.DarkMatter") %>%
+      addCircleMarkers(radius = 6, fillColor = ~pal(crime_count), fillOpacity = 1, stroke = FALSE,
+                       popup = ~paste0("<b>Park: </b>", Park_Name,
+                                       "<br><b>Between: ", input$date_range[1], " and ", input$date_range[2], "</b>",
+                                       "<br><b># Crimes within ", input$radius, " meters: </b>", crime_count)
+      ) %>%
+      addLegend(pal = pal, values = ~crime_count, title = "# Crimes in Zone")
+  })
+  
+  #create time series plot using dynamic radius and time range
+  time_series_crimes <- reactive({
+    generate_crime_timeseries(selected_parks = input$park_select, crime_radius = input$radius, 
+                              start_date = input$date_range[1], end_date = input$date_range[2])
+    })
+  #plot the crime data using ggplot
+  output$crime_timeseries <- renderPlot({
+    ts_crimes <- time_series_crimes()
+    ggplot(ts_crimes, aes(x = month)) +
+      geom_col(aes(y = crime_count), fill = "lightblue") +
+      geom_line(aes(y=crime_perc), color= "red", size=1) +
+      scale_y_continuous(name = "# Crimes Near a Park",
+                         sec.axis = sec_axis(~ . * 100/ max(ts_crimes$crime_count, na.rm = TRUE), 
+                                             name = "% Crimes Near a Park")) +
+      labs(x = "Month",
+        title = "Volume and Percentage of Crimes Inside Park Zones") +
+      theme_minimal()
+  })
+  
+  output$table1 <- DT::renderDataTable(crime)
+  
+  ###Danielle - tab2 ---------------------------------------------------------------------------------------------
   output$table2 <- DT::renderDataTable(licenses)
+  
+  ###Jake - tab3 ---------------------------------------------------------------------------------------------
   output$table3 <- DT::renderDataTable(parks)
+  
+  ###Erich - tab4 ---------------------------------------------------------------------------------------------
   output$table4 <- DT::renderDataTable(facilities)
+  
 }
 
 shinyApp(ui = ui, server = server)
--- East_Group2_App/data_prep.R
+++ East_Group2_App/data_prep.R
@@ -1,10 +1,143 @@
+#load libraries
 library(tidyverse)
+library(sf)
+library(leaflet)
 
-calls <- read.csv("311_Phone_Call_Log_Mod.csv")
-licenses <- read.csv("Business_Licenses.csv")
+#get parks data
 parks <- read.csv("Parks_Locations_and_Features.csv")
-facilities <- read.csv("Public_Facilities.csv")
 
+
+
+
+### Bridget -- Crime data ----------------------------------------------------------------------------
+#get crime data
+crime <- read.csv("SB_PoliceData.csv")
+
+#remove offense column since it is all NA
+crime_simpleB <- crime[c('lat','lon','date','street')]
+
+#simplify park data for this analysis
+parks_simpleB <- parks[c('Park_Name', "Lon", "Lat")] 
+
+
+## crime map code ##
+#convert both datasets to sf
+lat_lon_crs = 4326
+parks_sf <- st_as_sf(
+  parks_simpleB,
+  coords = c("Lon", "Lat"),
+  crs = lat_lon_crs
+)
+
+crime_sf <- st_as_sf(
+  crime_simpleB,
+  coords = c("lon", "lat"),
+  crs = lat_lon_crs
+)
+
+#project to meters to measure distance
+sb_utm = 32616 #utm code for South Bend gotten from the internet
+parks_proj  <- st_transform(parks_sf, sb_utm)
+crime_proj  <- st_transform(crime_sf, sb_utm)
+
+#confirm date column is the right type
+crime_proj$date <- as.Date(crime_proj$date)
+
+#function to create crime map with dynamic radius and start/end dates
+generate_crime_map <- function(crime_radius, start_date = NULL, end_date = NULL) {
+  #create parks with buffer by radius
+  parks_buffered <- parks_proj %>%
+    mutate(buffer = st_buffer(geometry, dist = crime_radius))
+  
+  #filter crimes by date
+  crimes_filtered <- crime_proj
+  if (!is.null(start_date) && !is.null(end_date)) {
+    crimes_filtered <- crime_proj %>%
+      filter(date >= start_date & date <= end_date)
+  }
+  
+  #join park and crime data
+  parks_with_crime <- st_join(
+    crimes_filtered,
+    parks_buffered %>% st_set_geometry("buffer"),
+    join = st_within
+  )
+
+  #calculate number of crimes per park and remove crimes not near a park
+  crime_counts <- parks_with_crime %>%
+    st_drop_geometry() %>%
+    count(Park_Name, name = "crime_count") %>%
+    filter(!is.na(Park_Name))
+
+  #join crime_count to df
+  parks_buffered <- parks_buffered %>%
+    left_join(crime_counts, by = "Park_Name") %>%
+    mutate(crime_count = replace_na(crime_count, 0))
+  
+  #convert back to use with leaflet
+  parks_map <- st_transform(parks_buffered, lat_lon_crs)
+  
+  #return map
+  return(parks_map)
+}
+
+## crime data series code ##
+generate_crime_timeseries <- function(selected_parks = NULL, crime_radius, start_date, end_date) {
+  #filter for selected parks
+  parks_filtered <- parks_proj
+  if (!is.null(selected_parks) && length(selected_parks) > 0) {
+    parks_filtered <- parks_filtered %>% filter(Park_Name %in% selected_parks)
+  }
+  
+  #filter for crime dates
+  crimes_filtered <- crime_proj %>%
+    filter(date >= start_date & date <= end_date)
+  
+  #create buffers around selected parks
+  parks_buffered <- parks_filtered %>%
+    st_buffer(dist = crime_radius)
+  
+  #get crimes that happened near a park
+  crimes_inside <- st_intersection(crimes_filtered, parks_buffered) %>%
+    st_drop_geometry() %>%
+    distinct(row_number())
+
+  #label crimes that happened near the park
+  crimes_filtered <- crimes_filtered %>%
+    mutate(location = ifelse(row_number() %in% row_number(crimes_inside), "Within", "Outside"))
+  
+  #find volume and percentage of crimes near parks
+  crime_ts <- crimes_filtered %>%
+    mutate(month = floor_date(date, "month")) %>%
+    group_by(month) %>%
+    summarise(crime_count = sum(location == "Within"), crime_perc = crime_count / n() * 100)
+  
+  #return timeseries
+  return(crime_ts)
+ 
+}
+
+
+###Danielle - Business License data  ----------------------------------------------------------------------------
+licenses <- read.csv("Business_Licenses.csv")
+
+
+
+
+
+###Jake - Parks(?) data  --------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+#Erich -- Facilities data  -------------------------------------------------------------------------------------
+facilities <- read.csv("Public_Facilities.csv")
 facilities <- facilities %>%
   mutate(
     POPL_ADDR1 = POPL_ADDR1 |>
